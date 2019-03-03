012cad760139641372f59be7226186db85defd67
inflatus <inflatus@gmail.com>
2019-03-03 11:52:05 -0500

    changed url to inflatus.io

--- Scraping/useragent.py
+++ Scraping/useragent.py
@@ -1,7 +1,7 @@
 import urllib.request
 
 try:
-    url = 'https://annarchive.com/dragon.html'
+    url = 'https://inflatus.io'
 
     headers = {}
     headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
