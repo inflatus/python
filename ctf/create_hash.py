bc87e2ca9f40c0e53c0d6e15e364cb7dff1c6fc0
inflatus <inflatus@gmail.com>
2019-09-07 18:18:19 -0400

    added md5 / need to revisit

--- ctf/create_hash.py
+++ ctf/create_hash.py
@@ -1,5 +1,7 @@
 #creating a sha256 hash from input
 #need to expand on this to provide hash choice
+#find a way to iterate over hashlib.algorithms_available
+#this script is inefficient
 
 import hashlib
 
@@ -10,9 +12,11 @@ encoded_sha224 = hashlib.sha224(input_hash.encode())
 encoded_sha256 = hashlib.sha256(input_hash.encode())
 encoded_sha384 = hashlib.sha384(input_hash.encode())
 encoded_sha512 = hashlib.sha512(input_hash.encode())
+encoded_md5 = hashlib.md5(input_hash.encode())
 
 print ('sha1 ',encoded_sha1.hexdigest())
 print ('sha224 ',encoded_sha224.hexdigest())
 print ('sha256 ',encoded_sha256.hexdigest())
 print ('sha384 ',encoded_sha384.hexdigest())
 print ('sha512 ',encoded_sha512.hexdigest())
+print ('md5 ',encoded_md5.hexdigest())
