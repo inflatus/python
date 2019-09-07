d182304c2bc5fd1411d122f0a61c7dd45eb74389
inflatus <inflatus@gmail.com>
2019-09-07 17:36:57 -0400

    added the rest of the sha hashes

--- ctf/create_hash.py
+++ ctf/create_hash.py
@@ -5,7 +5,14 @@ import hashlib
 
 input_hash = input('Select value to hash ')
 
-encoded_hash = hashlib.sha256(input_hash.encode())
+encoded_sha1 = hashlib.sha1(input_hash.encode())
+encoded_sha224 = hashlib.sha224(input_hash.encode())
+encoded_sha256 = hashlib.sha256(input_hash.encode())
+encoded_sha384 = hashlib.sha384(input_hash.encode())
+encoded_sha512 = hashlib.sha512(input_hash.encode())
 
-print (encoded_hash.hexdigest())
-
+print ('sha1 ',encoded_sha1.hexdigest())
+print ('sha224 ',encoded_sha224.hexdigest())
+print ('sha256 ',encoded_sha256.hexdigest())
+print ('sha384 ',encoded_sha384.hexdigest())
+print ('sha512 ',encoded_sha512.hexdigest())
