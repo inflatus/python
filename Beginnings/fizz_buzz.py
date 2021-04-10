aa5e434a95eec47463e6ce2187e9e08928cee3c7
inflatus <inflatus@gmail.com>
2021-04-10 09:34:27 -0400

    'reformatted'

--- Beginnings/fizz_buzz.py
+++ Beginnings/fizz_buzz.py
@@ -3,7 +3,7 @@ fizzbuzz = []
 start = int(input('Start Value:'))
 end = int(input('End Value:'))
 
-for i in range(start, end+1):
+for i in range(start, end + 1):
     entry = ''
     if i % 3 == 0:
         entry += 'Fizz'
