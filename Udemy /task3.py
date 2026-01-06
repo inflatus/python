3f24235c501d0440186675b2bae3a45a6a309cdf
inflatus <inflatus@gmail.com>
2026-01-05 19:02:46 -0500

    Still working

--- Udemy /task3.py
+++ Udemy /task3.py
@@ -1,3 +1,10 @@
-name = "Brad"
-print(name)
-print(len(name))
+# name = input("What is your name?")
+# print(name)
+# print(len(name))
+
+# print(len(input("What is your name?")))
+
+username = input("What is your username?")
+length = len(username)
+print(length)
+
