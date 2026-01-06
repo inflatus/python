c46d22819b6ba000ccb9cf84df63bea8ea544d4f
inflatus <inflatus@gmail.com>
2026-01-05 19:15:52 -0500

    End of Day 1

---
+++ Udemy /Band_Name_Generator.py
@@ -0,0 +1,7 @@
+# This is the first days app
+# The Bnad name generator
+# Will ask two questions and generate a name
+
+city = input("what's the name of the city you grew up in? ")
+pets_name = input("What's your pet's name? ")
+print("Your band's name could be "+city+ " "+pets_name)
---
+++ Udemy /glass.py
@@ -0,0 +1,9 @@
+# this is what i came up with
+# this works as well
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass1, glass2 = glass2, glass1
+print(glass1)
+print(glass2)
