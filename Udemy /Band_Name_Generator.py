b22b970b69e153625db57de8a3e528765b727bb0
inflatus <inflatus@gmail.com>
2026-01-05 19:17:09 -0500

    End of Day 1

--- Udemy /Band_Name_Generator.py
+++ Udemy /Band_Name_Generator.py
@@ -2,6 +2,7 @@
 # The Bnad name generator
 # Will ask two questions and generate a name
 
+print("Welcome to the Band Name Generator")
 city = input("what's the name of the city you grew up in? ")
 pets_name = input("What's your pet's name? ")
 print("Your band's name could be "+city+ " "+pets_name)
